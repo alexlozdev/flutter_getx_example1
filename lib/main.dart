b48a1acff632ffea8277fb9757e71069d2445329
Alex <61686168+alexlozdev@users.noreply.github.com>
2022-10-05 18:44:40 +0800

    change 1005

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("counter")),
+      appBar: AppBar(title: Text("counter-1005")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -24,7 +24,7 @@ class Home extends StatelessWidget {
               'clicks: ${controller.count}',
             )),
             ElevatedButton(
-              child: Text('Next Route'),
+              child: Text('Next Route-1005'),
               onPressed: () {
                 Get.to(Second());
                 /*
